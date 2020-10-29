e786fa7cb22b06521dae42bc3c91bd2799940ed2
Aishwarya <amuthyala511@gmail.com>
2020-10-29 12:48:13 +0530

    Welcome!! On master branch

---
+++ UserRegistrationUsingLambdaFunction/src/main/java/com/blz/lambdafunction/userregistration/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.blz.lambdafunction.userregistration;
+
+public class UserRegistration
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to User Registration Problem Using Lambda Function");
+	}
+}
